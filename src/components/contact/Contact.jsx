1fece2440ae28a5eea73a18a8b3dde9d8f85a531
Arsh <arsh@Arshs-MacBook-Air.local>
2023-08-09 21:32:43 +0530

    Portfolio

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -49,7 +49,7 @@ const Contact = () => {
             </div>
             <div className="c-info-item">
               <img className="c-icon" src={Address} alt="" />
-              Tang, Dharamsala, Kangra(Himachal Prades) - 176052
+              Dharamsala, Kangra(Himachal Prades) - 176052
             </div>
           </div>
         </div>
